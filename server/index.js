c0cb8ffe3547a08d06071e590e400c8ea29dfdc5
nadillaizt <abhinaya3403@gmail.com>
2024-03-08 15:43:31 +0700

    belajar server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const url = require("url");
+
+const PUBLIC_DIR = path.join(__dirname, "../public");
+const PORT = 3000;
+
+const server = (req, res) => {
+  // Lokalhost:8000
+  if (req.url === "/") {
+    res.end("INI HALAMAN SEARCH");
+  } else if (req.url === "/search") {
+    res.end("ini halaman search");
+  } else {
+    res.end("gak ada...");
+  }
+};
+
+http.createServer(server).listen(PORT);
+console.log(`Server is running ... PORT : localhost: ${PORT}`);
